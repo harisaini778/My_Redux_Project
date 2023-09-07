be4b9376e73f466b28ab29de2542102f5f64119e
Hari Kumar Saini <harikumarsaini778@gmail.com>
2023-09-07 12:27:21 +0530

    added a dispatch funnction and action in the code

--- redux-demo.js
+++ redux-demo.js
@@ -2,11 +2,20 @@ const redux = require("redux"); // import redux in this way using this syntax
 
 
 
-const counterReducer = (state  = {counter : 0}, action) => {
-    return {
-        counter: state.counter + 1
+const counterReducer = (state = { counter: 0 }, action) => {
+    if (action.type === "icrement") {
+        return {
+            counter: state.counter + 1
+        };
     }
- }; // reducer function it will receive state and action by default
+    if (action.type === "decrement") {
+        return {
+            counter: state.counter - 1
+        };
+    }
+    return state;
+}
+  // reducer function it will receive state and action by default
 
 const store = redux.legacy_createStore(counterReducer);
 
@@ -21,3 +30,4 @@ store.subscribe(counterSubscriber);
 
 store.dispatch({type:"increment"});
 
+store.despatch({ type: "decrement" });
